95e4e21018f133220dafa96af83ebf4a8ad1e4be
Calvin Teuber <calvin.teuber@volkswagen.de>
2024-04-23 18:28:26 +0200

    changes berichtigung.py to be object oriented

--- berichtigung.py
+++ berichtigung.py
@@ -1,87 +1,92 @@
 import csv
 from tabulate import tabulate
 
-brutto_data: dict[str, list] = {}
-netto_data: dict[str, list] = {}
-years: list[str] = []
 
-total_percentages: dict[str, list[float]] = {}
-averages: dict[str, float] = {}
-self_supply: dict[str, float] = {}
+class Serializer:
+    def __init__(self):
+        self.brutto_data: dict[str, list] = {}
+        self.netto_data: dict[str, list] = {}
+        self.years: list[str] = []
 
-row: list = []
-rows: list[[]] = []
+        self.percentages: dict[str, list[float]] = {}
+        self.average: dict[str, float] = {}
+        self.self_supply: dict[str, float] = {}
+
+    def read_csv(self):
+        with open("./data/netto.csv", "r", encoding="utf-8", newline="") as netto, open(
+                "./data/brutto.csv", "r", encoding="utf-8", newline="") as brutto:
+            netto_reader = csv.reader(netto, delimiter=";")
+            brutto_reader = csv.reader(brutto, delimiter=";")
+
+            self.process_netto(netto_reader)
+            self.process_brutto(brutto_reader)
+
+    def process_netto(self, reader):
+        for i, data in enumerate(reader):
+            if i == 0:
+                self.years = data[1:]
+                continue
+            self.netto_data[data[0]] = data[1:]
+            self.percentages[data[0]] = []
+
+    def process_brutto(self, reader):
+        for i, data in enumerate(reader):
+            if i == 0:
+                continue
+            self.brutto_data[data[0]] = data[1:]
+
+    def clean_up(self):
+        for key in self.brutto_data.keys():
+            self.brutto_data[key] = [string.replace(".", "").replace(",", ".") for string in self.brutto_data[key]]
+            self.netto_data[key] = [string.replace(".", "").replace(",", ".") for string in self.netto_data[key]]
+
+    def calc_percentages(self):
+        for i, year in enumerate(self.years):
+            year_sum: float = 0.0
+            for key, value in self.netto_data.items():
+                year_sum += float(value[i])
+            for key, value in self.netto_data.items():
+                self.percentages[key].append(float(value[i]) / year_sum * 100 if value[i] != "0" else 0)
+
+    def calc_averages(self):
+        for key, value in self.percentages.items():
+            self.average[key] = sum(value) / len(value)
+
+    def calc_self_supply(self):
+        for n_key, n_value in self.netto_data.items():
+            percentages = []
+            for i, value in enumerate(n_value):
+                if self.brutto_data[n_key][i] != "0":
+                    percentages.append(100 - (float(n_value[i]) / float(self.brutto_data[n_key][i]) * 100))
+            self.self_supply[n_key] = sum(percentages) / len(percentages)
+
+    def create_table(self):
+        header = self.years
+        header.insert(0, "Art")
+        header.append("Anteil (Mittelwert)")
+        header.append("Anteil Eigenbedarf (Mittelwert)")
+
+        rows = []
+        for key, value in self.percentages.items():
+            row: list[str] = [f"{val:.2f}%" for val in value]
+            row.insert(0, key)
+            row.append(f"{self.average[key]:.2f}%")
+            row.append(f"{self.self_supply[key]:.2f}%")
+            rows.append(row)
+
+        table = tabulate(rows, headers=header)
+        with open("result.txt", "w") as result:
+            result.write(table)
+
+    def run(self):
+        self.read_csv()
+        self.clean_up()
+        self.calc_percentages()
+        self.calc_averages()
+        self.calc_self_supply()
+        self.create_table()
 
 
-def extract_brutto_data(reader, brutto_data):
-    list_years: list[str] = []
-    for i, line in enumerate(reader):
-        if i == 0:
-            list_years = line[1:]
-        else:
-            brutto_data[line[0]] = line[1:]
-    return list_years
-
-
-def extract_netto_data(reader, total_percentages, netto_data):
-    for i, line in enumerate(reader):
-        if i == 0:
-            continue
-        netto_data[line[0]] = line[1:]
-        total_percentages[line[0]] = []
-
-
-with open("./data/brutto.csv", "r", encoding="utf-8") as brutto, open(
-        "./data/netto.csv", "r", encoding="utf-8") as netto:
-    brutto_reader = csv.reader(brutto, delimiter=";")
-    netto_reader = csv.reader(netto, delimiter=";")
-
-    years = extract_brutto_data(brutto_reader, brutto_data)
-    extract_netto_data(netto_reader, total_percentages, netto_data)
-
-for key in brutto_data.keys():
-    brutto_data[key] = [string.replace(".", "").replace(",", ".") for string in brutto_data[key]]
-    netto_data[key] = [string.replace(".", "").replace(",", ".") for string in netto_data[key]]
-
-# Aufgabe 1
-for i, year in enumerate(years):
-    year_sum = 0.0
-    for value in netto_data:
-        year_sum += float(netto_data[value][i])
-    for key, data in netto_data.items():
-        total_percentages[key].append((float(data[i]) / year_sum * 100 if data[i] != "0" else 0))
-
-# Aufgabe 2
-for key, value in total_percentages.items():
-    averages[key] = sum(value) / len(total_percentages[key])
-
-# Aufgabe 3
-for value in netto_data:
-    percentage_sum = 0.0
-    for i, data in enumerate(netto_data[value]):
-        if netto_data[value][i] != '0' and brutto_data[value][i] != '0':
-            brutto_val = float(brutto_data[value][i])
-            netto_val = float(netto_data[value][i])
-
-            percentage_sum += ((brutto_val / netto_val) * 100) - 100
-    self_supply[value] = (percentage_sum / len(netto_data[value]))
-
-# create header
-header = years
-header.insert(0, 'Art')
-header.append('Anteil (Mittelwerte)')
-header.append('Anteil Eigenbedarf(Mittelwerte)')
-
-# create row structure
-for name in netto_data.keys():
-    row: list[str] = [f"{val:.2f}%" for val in total_percentages[name]]
-    row.insert(0, name)
-    row.append(f"{averages[name]:.2f}%")
-    row.append(f"{self_supply[name]:.2f}%")
-
-    rows.append(row)
-
-# create table
-table = tabulate(rows, headers=header, tablefmt='github')
-with open("result.txt", "w") as result:
-    result.write(table)
+if __name__ == "__main__":
+    serializer = Serializer()
+    serializer.run()
